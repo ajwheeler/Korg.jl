d92e305ced7fc3021f3fb347a9210cc440ec1a74
Matthew Abruzzo <matthewabruzzo@gmail.com>
2021-02-25 23:59:30 -0500

    Introduced a file for storing physical constants.

---
+++ src/constants.jl
@@ -0,0 +1,11 @@
+# make things more self-consistent later
+
+const kboltz_cgs = 1.380649e-16 # erg/K
+const hplanck_cgs = 6.62607015e-27 # erg*s
+
+const electron_charge_cgs = 4.80320425e-10 # statcoulomb or cm^3/2 * g^1/2 / s
+
+const kboltz_eV = 8.617333262145e-5 #eV/K
+const hplanck_eV = 4.135667696e-15 #eV*s
+const RydbergH_eV = 13.595 #eV - this could be updated
+
