b7883f24f2389c4386d140f481bef02c78bc48f9
Adam Wheeler <a.wheeler@columbia.edu>
2021-04-28 10:59:01 -0600

    add favicon

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,16 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, SSSynth
 
-makedocs(sitename="SSSynth Documentation", 
+makedocs(sitename="SSSynth", 
          modules=[SSSynth],
          pages=[
                 "Quickstart" => "index.md"
                 "All Functions" => "API.md"
                 "References" => "refs.md"
-               ])
+               ],
+        authors="Adam Wheeler and Matthew Abruzzo",
+        format=Documenter.HTML(assets=["assets/favicon.ico"])
+       )
 
 deploydocs(repo = "github.com/ajwheeler/SSSynth.jl.git",
            devbranch="main")
