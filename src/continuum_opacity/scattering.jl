4469384300f181d8fa2f5d964e74b9a0da46d2b2
Adam Wheeler <adamwhlr@gmail.com>
2021-10-05 14:01:08 -0400

    move electron scattering out of module file; clarify sources.

--- src/continuum_opacity/scattering.jl
+++ src/continuum_opacity/scattering.jl
@@ -4,13 +4,13 @@
 Absorption coefficient from Rayleigh scattering by neutral H and He.  Formulation taken from Colgan+
 2016.  The formulation for H is adapted from Lee 2005, which states that it is applicable redward of 
 Lyman alpha. The formulation for He is adapted from Dalgarno 1962 and Dalgarno & Kingston 1960, and 
-is applicable redward of 1300 Å.
+the formulation for H2 is from Dalgarno and Williams 1963 and is applicable redward of 1300 Å.
 """
-function rayleigh(λs::AbstractVector{<:Real}, nH_I, nHe_I, nH2)
+function rayleigh(νs::AbstractVector{<:Real}, nH_I, nHe_I, nH2)
     σth = 6.65246e-25 #Thompson scattering cross section [cm^2]
 
     #(ħω/ 2E_H)^2 in Colgan+ 2016.  The photon energy over 2Ryd
-    E_2Ryd_2 = @. (hplanck_eV * c_cgs / (2 * Rydberg_eV * λs))^2
+    E_2Ryd_2 = @. (hplanck_eV * νs / (2 * Rydberg_eV ))^2
     E_2Ryd_4 = E_2Ryd_2.^2
     E_2Ryd_6 = E_2Ryd_2.*E_2Ryd_4
     E_2Ryd_8 = E_2Ryd_4.^2
@@ -23,7 +23,7 @@ function rayleigh(λs::AbstractVector{<:Real}, nH_I, nHe_I, nH2)
     αH_HE = @. (nH_I*σH_σth + nHe_I*σHe_σth) * σth
 
     #Dalgarno & Williams 1962 equation 3 (which assumes λ in Å)
-    invλ2 = 1 ./ ((1e8*λs) .^ 2)
+    invλ2 = (νs ./ (1e8 * c_cgs)) .^2
     invλ4 = invλ2 .^ 2
     invλ6 = invλ2 .* invλ4
     invλ8 = invλ4 .^ 2
@@ -31,4 +31,21 @@ function rayleigh(λs::AbstractVector{<:Real}, nH_I, nHe_I, nH2)
 
     αH_HE .+ αH2
 end
+
+"""
+    electron_scattering(nₑ, ρ)
+
+Compute the opacity from scattering off of free electrons. This has no wavelength dependence. It
+assumes isotropic scattering
+
+# Arguments
+- `nₑ::F`: number density of free electrons (in cgs)
+- `ρ::F`: the mass density
+
+# Notes
+I adopted the formula described in section 5.12 of Kurucz (1970) and the equation in the electron
+scattering subsection of Gray (2005); the actual coefficient value comes from the latter. It turns
+out that the coefficient in Kurucz (1970) has a typo (it's a factor of 10 too large).
+"""
+electron_scattering(nₑ::F, ρ::F) where {F<:Real} = 0.6648e-24*nₑ/ρ
 
