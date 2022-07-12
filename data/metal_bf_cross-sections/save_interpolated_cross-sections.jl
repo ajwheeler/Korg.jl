6100f568040e3da26fa1cdf4a14009a776ccda03
Adam Wheeler <a.wheeler@columbia.edu>
2022-07-12 17:58:40 -0400

    save cross-section data to h5 file

---
+++ data/metal_bf_cross-sections/save_interpolated_cross-sections.jl
@@ -0,0 +1,56 @@
+using Korg, HDF5, Base
+
+include("NORAD_TOPBase_parsing_code.jl")
+
+DATA_DIR = ARGS[1]
+OUT_FILE = ARGS[2]
+
+function save_bf_cross_section(f, spec, logTs, νs, ξ=2e5)
+    @assert issorted(νs, rev=true) # should be decreasing freq
+    
+    Ts = 10 .^ logTs
+    wls = Korg.c_cgs ./ νs
+    σs = cross_section_bf(spec, wls, Ts, DATA_DIR)
+    
+    Rs = Korg.c_cgs ./ sqrt.(2 * Korg.kboltz_cgs * Ts ./ Korg.get_mass(Korg.species"He I") .+ ξ^2) 
+    λs = Korg.c_cgs ./ νs # the frequencecy grid is linear-enough in wavelength over the ~ 1 Å scale LSF 
+
+    # convert to megabarns and cast to single precision
+    σs = Float32.(hcat(Korg.constant_R_LSF.(eachcol(σs), Ref(λs), Rs)...) .* 1e18)
+    mask = σs .< floatmin(Float32)
+    σs[mask] .= 0
+    print(minimum(σs), " -- ", maximum(σs), " ($(sum(mask)) denormal) ")
+    
+    # save in order of increasing freq / decreasing wl
+    f["cross-sections/"*string(spec), shuffle=(), deflate=9] = reverse(σs, dims=1)
+end
+
+logTs = 2:0.1:5
+wl_lo = 490 * 1e-8
+wl_hi = 30_050 * 1e-8
+
+# descending freq, ascending wl
+νs = Korg.c_cgs / wl_lo : -1e11 : Korg.c_cgs / wl_hi
+
+filename = OUT_FILE
+h5open(filename, "w") do f
+    f["logT_min"]  = logTs[1]
+    f["logT_step"] = step(logTs)
+    f["logT_max"] = logTs[end]
+    f["nu_min"] = νs[end]
+    f["nu_step"] = -step(νs)
+    f["nu_max"] = νs[1]
+
+    atomic_numbers = [1:14... ; 16:2:20... ; 26]
+    @time for Z in atomic_numbers
+        for ionization_number in [1, 2]
+            if Z == 1 && ionization_number == 2
+                continue # no such thing as H II 
+            end
+            spec = Korg.Species(Korg.atomic_symbols[Z] * " $ionization_number")
+            println(spec)
+            save_bf_cross_section(f, spec, logTs, νs)
+        end
+    end
+end
+;
--- src/ContinuumAbsorption/ContinuumAbsorption.jl
+++ src/ContinuumAbsorption/ContinuumAbsorption.jl
@@ -14,9 +14,10 @@ include("absorption_He.jl")
 include("absorption_ff_positive_ion.jl")
 include("scattering.jl")
 
-# the following are only imported for computing experimental metal bf continuum opacities
-using ..Korg: partition_funcs, Species, ismolecule, get_atoms, _data_dir
-include("absorption_metal.jl")
+#TODO
+## the following are only imported for computing experimental metal bf continuum opacities
+#using ..Korg: partition_funcs, Species, ismolecule, get_atoms, _data_dir
+#include("absorption_metal.jl")
 
 export total_continuum_absorption
 
