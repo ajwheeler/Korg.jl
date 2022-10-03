1d7062c8a87ba4830c54f744432f1a6b6624f554
Adam Wheeler <wheeler.883@osu.edu>
2022-10-03 09:54:32 -0400

    check A_X length

--- src/synthesize.jl
+++ src/synthesize.jl
@@ -105,8 +105,8 @@ function synthesize(atm::ModelAtmosphere, linelist, A_X::Vector{<:Real}, λs::Ab
         throw(ArgumentError("λs must be in increasing order."))
     end
 
-    if A_X[1] != 12
-        throw(ArgumentError("A(H) must be 12."))
+    if length(A_X) != Natoms || (A_X[1] != 12)
+        throw(ArgumentError("A(H) must be a 92-element vector with A[1] == 12."))
     end
 
     abs_abundances = @. 10^(A_X - 12) # n(X) / n_tot
