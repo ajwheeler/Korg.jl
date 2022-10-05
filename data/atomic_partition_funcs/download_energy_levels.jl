92be019d1bed8facd2a17168847cddadbd3bf7e8
Adam Wheeler <wheeler.883@osu.edu>
2022-10-05 15:26:39 -0400

    fix a small bug with the partition functions that mostly effected hydrogen.  I was due to the difficult of parsing NIST energy levels.  I've added a test comparing to the Barlem & COllet values for a few species, but those may have issues.

--- data/atomic_partition_funcs/download_energy_levels.jl
+++ data/atomic_partition_funcs/download_energy_levels.jl
@@ -5,13 +5,12 @@ using Korg, Downloads, CSV, DataFrames
 
 function download_levels_from_NIST()
     dfs = Dict()
-    for elem in Korg.atomic_symbols, ionization in ["I", "II", "III"]
-        if (elem == "H" && ionization != "I") || (elem == "He" && ionization == "III")
+    for spec in Korg.all_atomic_species()
+        if spec in Korg.Species.(["H II", "He III"])
             continue
         end
-        
-        species = elem*"+"*ionization
-        
+
+        species = replace(string(spec), ' '=>'+')
         url = "https://physics.nist.gov/cgi-bin/ASD/energy1.pl?de=0&spectrum=$(species)&units=1&format=2&output=0&page_size=15&multiplet_ordered=1&conf_out=on&level_out=on&g_out=on&temp=&submit=Retrieve+Data"
         df = CSV.read(Downloads.download(url), DataFrame, header=["configuration", "g", "level", "c3"], skipto=2)
         
@@ -20,15 +19,17 @@ function download_levels_from_NIST()
             parse(Float64, replace(l, r"\+."=>"", "&dagger;"=>"", (collect("\"[]()=au?") .=> "")...))
         end
         select!(df, ["configuration", "g", "level"])
+        filter!(df) do row #we don't want any "summary" rows
+            ! isnothing(match(r"[a-z]", row.configuration))
+        end
         dropmissing!(df)
-        df = df[df.configuration .!= "=\"\"", :]
         
         df.n = map(df.configuration) do conf
             #last instance of an integer after either " or .
             parse(Int, (collect(eachmatch(Regex("[\"\\.](\\d+)"), conf))[end].captures[1]))
         end
         
-        dfs[Korg.Species("$elem $ionization")] = df
+        dfs[spec] = df
     end
     dfs
 end
