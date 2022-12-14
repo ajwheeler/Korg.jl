09bc2bcf9ddf1e4b292127ac24a612ba43669990
Adam Wheeler <wheeler.883@osu.edu>
2022-12-14 15:03:36 -0500

    working interpolation

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -150,3 +150,101 @@ function read_model_atmosphere(fname::AbstractString) :: ModelAtmosphere
         end
      end
 end
+
+module InterpolateSDSSMARCS 
+using HDF5: h5read
+using ..Korg: PlanarAtmosphere, PlanarAtmosphereLayer, ShellAtmosphere, ShellAtmosphereLayer
+
+atmosphere_archive = "../../korg_files/atmospheres/SDSS_MARCS_atmospheres.h5"
+
+#load from file
+planar_exists = h5read(atmosphere_archive, "planar/exists")
+planar_grid = h5read(atmosphere_archive, "planar/grid")
+planar_nodes = [h5read(atmosphere_archive, "planar/grid_values/$i") for i in 1:5]
+spherical_exists = h5read(atmosphere_archive, "spherical/exists")
+spherical_grid = h5read(atmosphere_archive, "spherical/grid")
+R_grid = h5read(atmosphere_archive, "spherical/R_grid")
+spherical_nodes = [h5read(atmosphere_archive, "spherical/grid_values/$i") for i in 1:5]
+
+function interpolate_marcs(Teff, logg, metallicity=0, alpha=0, carbon=0;
+        spherical=logg < 3.5,
+        nodes=(spherical ? spherical_nodes : planar_nodes),
+        exists=(spherical ? spherical_exists : planar_exists),
+        grid=(spherical ? spherical_grid : planar_grid),
+        R_grid=(spherical ? R_grid : nothing))
+
+    #TODO calculate R using M = 1
+    
+    params = [Teff, logg, metallicity, alpha, carbon]
+    
+    upper_vertex = map(zip(params, nodes)) do (p, p_nodes)
+        @assert p_nodes[1] <= p <= p_nodes[end]
+        findfirst(p .<= p_nodes)
+    end
+    isexact = params .== getindex.(nodes, upper_vertex) #which params are on grid points?
+    
+    # allocate 2^n cube for each quantity
+    dims = Tuple(2 for _ in upper_vertex) #dimensions of 2^n hypercube
+    structure_type = typeof(promote(Teff, logg, metallicity, alpha, carbon)[1])
+    structure = Array{structure_type}(undef, (56, 5, dims...))
+    if spherical
+        R = Array{structure_type}(undef, dims)
+    else
+        R = [0]
+    end
+     
+    #put bounding atmospheres in 2^n cube
+    for I in CartesianIndices(dims)
+        local_inds = collect(Tuple(I))
+        atm_inds = copy(local_inds)
+        atm_inds[isexact] .= 2 #use the "upper bound" as "lower bound" if the param is on a grid point
+        atm_inds .+= upper_vertex .- 2
+
+        if !exists[atm_inds...] #return nothing if any required nodes don't exist
+            println(getindex.(nodes, atm_inds), " doesn't exist") 
+            return 
+        end
+        
+        structure[:, :, local_inds...] .= grid[:, :, atm_inds...]
+        spherical && (R[local_inds...] = R_grid[atm_inds...])
+    end
+
+    for i in 1:length(params) #loop over Teff, logg, etc.
+        isexact[i] && continue #no need to do anything for exact params
+        
+        # the bounding values of the parameter you are currently interpolating over 
+        p1 = nodes[i][upper_vertex[i]-1]
+        p2 = nodes[i][upper_vertex[i]]
+        
+        # inds1 and inds2 are the expressions for the slices through the as-of-yet 
+        # uninterpolated quantities (temp, logPg, etc) for each node value of the
+        # quantity being interpolated
+        # inds1 = (1, 1, 1, ..., 1, 1, :, :, ...)
+        # inds2 = (1, 1, 1, ..., 1, 2, :, :, ...)
+        inds1 = vcat([1 for _ in 1:i-1], 1, [Colon() for _ in i+1:length(params)])
+        inds2 = vcat([1 for _ in 1:i-1], 2, [Colon() for _ in i+1:length(params)])
+        
+        x = (params[i] - p1) / (p2 - p1) #maybe try using masseron alpha later
+        for structure_ind in 1:5 #TODO fix
+            structure[:, structure_ind, inds1...] = (1-x)*structure[:, structure_ind, inds1...] + x*structure[:, structure_ind, inds2...]
+        end
+        if spherical
+            R[inds1...] = (1-x)*R[inds1...] + x*R[inds2...]
+        end
+    end
+    
+    Float64.(structure[:, :, ones(Int, length(params))...]), Float64(R[1])
+end
+
+function assemble_atm((m, R))
+    m, R = Float64.(m), Float64(R)
+    if R == 0
+        PlanarAtmosphere(PlanarAtmosphereLayer.(m[:, 4], sinh.(m[:, 5]), m[:, 1],
+                                                          exp.(m[:, 2]), exp.(m[:, 3])))
+    else
+        ShellAtmosphere(ShellAtmosphereLayer.(m[:, 4], sinh.(m[:, 5]), m[:, 1],
+                                                        exp.(m[:, 2]), exp.(m[:, 3])), R)
+    end
+end
+
+end
