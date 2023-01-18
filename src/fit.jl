53111b3dba4506b386497d23e4f04886d53ace0e
Adam Wheeler <wheeler.883@osu.edu>
2023-01-18 12:39:00 -0500

    add poor method where one param is fit at a time

--- src/fit.jl
+++ src/fit.jl
@@ -76,11 +76,13 @@ function find_best_params(obs_wls, data, err, R, linelist;
         end
     end 
 
-    @time result = optimize(chi2, scale(p0), BFGS(linesearch=LineSearches.BackTracking()),  
-                            Optim.Options(time_limit=3600, store_trace=true, extended_trace=true),
-                            autodiff=:forward)
+    #@time result = optimize(chi2, scale(p0), BFGS(linesearch=LineSearches.BackTracking()),  
+    #                        Optim.Options(x_tol=1e-5, time_limit=3600, store_trace=true, extended_trace=true),
+    #                        autodiff=:forward)
 
-    #@time J0 = ForwardDiff.jacobian(global_synth, scale(p0))
+    @time J0 = ForwardDiff.jacobian(global_synth, scale(p0))
+    fit_each_param_locally(J0, obs_wls, data, err, linelist, p0, synthesis_wls, LSF_matrix, verbose)
+
     #grad2 = sum(J0.^2, dims=2)
     #wl_chunck_edges = first(obs_wls) : 20 : last(obs_wls)
     #lb, ub = 1, 1
@@ -93,11 +95,47 @@ function find_best_params(obs_wls, data, err, R, linelist;
     #end
     #lb, ub, _ = mean_grad2[argmax(last.(mean_grad2))]
     #verbose && @info "the subspectrum with the strongest sensitivity to the parameters is $(obs_wls[lb]) – $(obs_wls[ub]) Å"
+    #find_best_params_locally(lb, ub, obs_wls, data, err, linelist, p0, synthesis_wls, LSF_matrix, 
+    #                         verbose)
 
     #grad2, lb, ub
 end
 
-function find_best_params_locally(lb, ub, obs_wls, data, err, R, linelist, p0, synthesis_wls, 
+function fit_each_param_locally(J, obs_wls, data, err, linelist, p0, synthesis_wls, LSF_matrix, verbose; wl_buffer=10)
+    wl_chunck_edges = first(obs_wls) : 20 : last(obs_wls)
+    lb, ub = 1, 1
+    map(eachindex(p0)) do i
+        mean_J = []
+        for wl_ind in eachindex(wl_chunck_edges[1:end-1])
+            wl_lb = wl_chunck_edges[wl_ind]
+            wl_ub = wl_chunck_edges[wl_ind+1]
+            lb, ub = Korg.move_bounds(obs_wls, lb, ub, (wl_lb+wl_ub)/2, step(wl_chunck_edges))
+            push!(mean_J, (lb, ub, mean(J[lb:ub, i])))
+        end
+        lb, ub, _ = mean_J[argmax(last.(mean_J))]
+        verbose && @info "the subspectrum most sensitive to parameter $(i) is $(obs_wls[lb]) – $(obs_wls[ub])"
+
+        synth_wl_lb = findfirst(synthesis_wls .> obs_wls[lb]-wl_buffer)
+        synth_wl_ub = findfirst(synthesis_wls .> obs_wls[ub]+wl_buffer) - 1
+        small_synthesis_wls = synthesis_wls[synth_wl_lb:synth_wl_ub]
+        small_LSF_matrix = LSF_matrix[lb:ub, synth_wl_lb:synth_wl_ub]
+
+        chi2 = let obs_wls=obs_wls[lb:ub], data=data[lb:ub], err=err[lb:ub], p=scale(p0), i=i,
+                   synthesis_wls=small_synthesis_wls, LSF_matrix=small_LSF_matrix, linelist=linelist
+            scaled_p_i -> begin
+                p = [p[1:i-1] ; scaled_p_i ; p[i+1:end]]
+                flux = synth(synthesis_wls, obs_wls, p, linelist, LSF_matrix)
+                sum(((flux .- data)./err).^2)
+            end
+        end 
+
+        @time result = optimize(chi2, scale(p0)[i:i], BFGS(linesearch=LineSearches.BackTracking()),  
+                                Optim.Options(time_limit=1000, store_trace=true, extended_trace=true))
+                                #autodiff=:forward)
+    end
+end
+
+function find_best_params_locally(lb, ub, obs_wls, data, err, linelist, p0, synthesis_wls, 
                                   LSF_matrix, verbose; wl_buffer=10)
     # synthesize on a range which overshoots the subspectrum by wl_buffer Å on each side
     synth_wl_lb = findfirst(synthesis_wls .> obs_wls[lb]-wl_buffer)
