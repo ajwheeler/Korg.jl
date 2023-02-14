90a3d218c3c0bfd2c780c32440d60466690ebf5a
Adam Wheeler <wheeler.883@osu.edu>
2023-02-14 08:20:50 -0500

    credit MARCS better

--- src/atmosphere.jl
+++ src/atmosphere.jl
@@ -241,13 +241,13 @@ end
     interpolate_marcs(Teff, logg, Fe_M=0, alpha_M=0, C_M=0; kwargs...)
     interpolate_marcs(Teff, logg, A_X; kwargs...)
 
-Returns a model atmosphere obtained by interpolating the SDSS atmosphere grid, which provides 
+Returns a model atmosphere obtained by interpolating the MARCS SDSS atmosphere grid, which provides 
 atmospheres for varying values of ``T_\\mathrm{eff}``, ``\\log g`, [metals/H], [alpha/metals], 
 and [C/metals].  If the `A_X` (a vector of abundances in the format returned by [`format_A_X`](@ref)
 and accepted by [`synthesize`](@ref)) is provided instead of `M_H`, `alpha_M`, and `C_M`, the 
 solar-relative ratios will be reconstructed assuming Grevesse+ 2007 solar abundances.
 
-The default model atmosphere grid is a repacked version of the 
+The model atmosphere grid is a repacked version of the 
 [MARCS SDSS grid](https://dr17.sdss.org/sas/dr17/apogee/spectro/speclib/atmos/marcs/MARCS_v3_2016/Readme_MARCS_v3_2016.txt).
 Before you use `interpolate_marcs` for the first time, you will have to download the grid with by 
 running `download_atmosphere_archive()`.  By default, the archive is stored at 
