6dc26b4660f010b5dc43a84d546c0fae355c2d3e
Charlesking Spike Y. Arphience <132521679+CharleskingSpikeArphience@users.noreply.github.com>
2023-05-14 22:42:46 -0400

    Update Solar Elemental Abundance(Magg 2022) .jl

--- src/Solar Elemental Abundance(Magg 2022) .jl
+++ src/Solar Elemental Abundance(Magg 2022) .jl
@@ -6,55 +6,7 @@ Solar_Elemental_Abundance = [12.0, 10.94, 3.31, 1.44, 2.8, 8.56, 7.98, 8.77, 4.4
                              2.083, 0.712, NaN, NaN, NaN, NaN, NaN, NaN ,0.116, NaN, -0.461 ]
 
 
-count = 0
 
-for i in Solar_Elemental_Abundance
-    print(i ," ")
-    count += 1
-
-end
-    
-println("\nnumber of elements listed: ", count)
-
-
-
-# Derived from the results of Ekaterina Magg's 2022 paper, with the statistics from Katharina Lodders' 2002 paper.
-# using thr equation: A_i = 1.57 + log10(N_i)
-
-using PyPlot
-using Korg
-
-n = [0,1,2,3,4]
-
-
-number = []
-
-count = 0
-
-for i in Solar_Elemental_Abundance
-    count += 1
-    push!(number, count)
-end
-
-label = []
-
-count1 = 1
-
-for i in 1:count
-    push!(label, Korg.atomic_symbols[count1])
-    count1 += 1
-end
-    
-
-plt_1 = plt.figure(figsize=(22,8))
-plt.scatter(number, Solar_Elemental_Abundance,s = 15, c = "blue")
-plt.xlabel("Z (species)")
-plt.ylabel("A(x)")
-plt.yticks([-5.0,-2.5,0.0,1,2,3,4,5,6,7,8,9,10,11,12])
-
-for i in range(1,length(number))
-        plt.annotate(label[i],(number[i]-0.4,Solar_Elemental_Abundance[i]+0.2))
-end
 
 
 
