b4d29bf7afbf1d747f07c18801a6fcf48c67224d
Adam Wheeler <wheeler.883@osu.edu>
2023-08-03 16:54:31 -0400

    block parsing of kurucz molecular linelists because of isotopes

--- src/linelist.jl
+++ src/linelist.jl
@@ -249,6 +249,7 @@ function parse_kurucz_linelist(f; vacuum=false)
 end
 
 function parse_kurucz_molecular_linelist(f; vacuum=false)
+    @error "Kurucz linelists are not yet supported for molecules"
     lines = Line[]
     for row in eachline(f)
         row == "" && continue #skip empty lines
