e5baed2e77b289c063354775fd4d433cbb998a9f
Adam Wheeler <wheeler.883@osu.edu>
2023-12-04 10:29:54 -0500

    add one more reference

--- src/linelist.jl
+++ src/linelist.jl
@@ -544,6 +544,8 @@ end
 The GALAH [DR 3](https://www.galah-survey.org/dr3/overview/)
 linelist (also used for [DR 4](https://github.com/svenbuder/GALAH_DR4)). 
 It ranges from roughly 4,675 Å to 7,930 Å.
+This linelist is based on, but distinct from 
+[Heiter 2021](https://ui.adsabs.harvard.edu/abs/2021A%26A...645A.106H/).
 See [Buder et al. 2021](https://ui.adsabs.harvard.edu/abs/2021MNRAS.506..150B%2F/abstract) for 
 details.
 """
