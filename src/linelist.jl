eb69c9769c89388b3d91f5183d7a250c1fc8037c
Adam Wheeler <wheeler.883@osu.edu>
2023-12-04 10:28:19 -0500

    add references to docstring

--- src/linelist.jl
+++ src/linelist.jl
@@ -541,7 +541,11 @@ end
 """
     get_GALAH_DR3_linelist()
 
-The GALAH DR 3 linelist.  It ranges from roughly 4,675 Å to 7,930 Å.
+The GALAH [DR 3](https://www.galah-survey.org/dr3/overview/)
+linelist (also used for [DR 4](https://github.com/svenbuder/GALAH_DR4)). 
+It ranges from roughly 4,675 Å to 7,930 Å.
+See [Buder et al. 2021](https://ui.adsabs.harvard.edu/abs/2021MNRAS.506..150B%2F/abstract) for 
+details.
 """
 function get_GALAH_DR3_linelist()
     path = joinpath(_data_dir, "linelists", "GALAH_DR3", "galah_dr3_linelist.h5")
