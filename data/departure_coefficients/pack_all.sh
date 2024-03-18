ffda22947bf57d18b55cc80b4313e60ae55ae716
Adam Wheeler <wheeler.883@osu.edu>
2024-03-18 13:39:46 -0400

    proper extension

--- data/departure_coefficients/pack_all.sh
+++ data/departure_coefficients/pack_all.sh
@@ -3,5 +3,5 @@ DIR=~/Dropbox/departure_coefficients/amarsi
 #for el in Al Ba C Ca Fe H K Li Mg Na O Si Ti
 for el in Mg Na O Si Ti
 do
-    julia pack_amarsi_coefficients.jl $DIR/$el $el.hdf5
+    julia pack_amarsi_coefficients.jl $DIR/$el $el.h5
 done
