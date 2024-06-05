dd767e41619919362a552bb8e9b718fad7eb38b3
andrew-saydjari <saydjari@g.harvard.edu>
2024-06-05 00:49:42 -0600

    using ≈ and atol style choice

--- test/qfactors.jl
+++ test/qfactors.jl
@@ -1,6 +1,6 @@
 @testset "qfactors" begin
     # Check a Q factor gives the right RV precision
-    @test abs(Korg.precRV_fromQ(1100,100,8000)-30.470741601352298) < 1e-10
+    @test Korg.precRV_fromQ(1100,100,8000) ≈ 30.470741601352298 atol=1e-10
 
     # Check that a solar spectrum at APOGEE resolution gives the expected Q factor
     delLog = 6e-6; 
@@ -19,12 +19,12 @@
     kout = synthesize(atm, apolines, A_X, wl_lo, wl_hi, wl_step, vmic=0, hydrogen_lines=true, hydrogen_line_window_size=300, electron_number_density_warn_threshold=1e10);
     tspec = (kout.flux)./(kout.cntm);
     Q = Korg.Qfactor(tspec, x_model, wavetarg, LSF_model)
-    @test abs(Q-1182.420317367743)<1e-10
+    @test Q ≈ 1182.420317367743 atol=1e-10
 
     msk = ones(Bool,length(wavetarg))
     msk[1:100].=false
     msk[end-100:100].=true
-    @test abs(Korg.precRV_MeasuredNoise(tspec, x_model, wavetarg, LSF_model, 1 ./100*ones(length(wavetarg)),msk_lres=msk)-29.490659698846365)<1e-10
-    @test abs(Korg.precRV_fromQ(Q,100,count(msk)).-27.34006821159959)<1e-10
+    @test Korg.precRV_MeasuredNoise(tspec, x_model, wavetarg, LSF_model, 1 ./100*ones(length(wavetarg)),msk_lres=msk) ≈ 29.490659698846365 atol=1e-10
+    @test Korg.precRV_fromQ(Q,100,count(msk)) ≈ 27.34006821159959 atol=1e-10
     # I think these two should agree... but the problem must be from the continuum defintion and how that propagates to S/N
 end
