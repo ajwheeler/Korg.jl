539d5f478330b2e4070b0f22d3a6689460d076f6
Adam Wheeler <awheeler@flatironinstitute.org>
2024-10-25 16:10:30 -0400

    pass each abund as a kwarg

--- src/synth.jl
+++ src/synth.jl
@@ -3,15 +3,20 @@ function synth(;
                logg=4.5,
                metals_H=0.0,
                alpha_H=0.0,
-               abundances=Dict(),
                linelist=get_VALD_solar_linelist(),
                wavelengths=(5000, 6000),
                rectify=true,
                R=Inf,
                vsini=0,
-               synthesize_kwargs...)
-    A_X = format_A_X(metals_H, alpha_H, abundances)
+               kwargs...,)
+    # if a keyword argument is an atomic symbol, it is assumed to be an abundance
+    # otherwise, it is passed to synthesize
+    abunds_set = [k for k in keys(kwargs) if k in atomic_symbols]
+
+    A_X = format_A_X(metals_H, alpha_H, Dict(k => kwargs[k] for k in abunds_set))
     atm = interpolate_marcs(Teff, logg, A_X)
+
+    synthesize_kwargs = [k => kwargs[k] for k in keys(kwargs) if !(k in abunds_set)]
     spectrum = synthesize(atm, linelist, A_X, wavelengths; synthesize_kwargs...)
     flux = if rectify
         spectrum.flux ./ spectrum.cntm
@@ -24,5 +29,5 @@ function synth(;
     if vsini > 0
         flux = apply_rotation(flux, spectrum.wavelengths, vsini)
     end
-    spectrum.wavelengths, flux
+    spectrum.wavelengths, flux, spectrum.cntm
 end
