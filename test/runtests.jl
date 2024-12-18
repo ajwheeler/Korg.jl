d89b796880598c4ebd524c017af8c02bbceafe6a
Adam Wheeler <awheeler@flatironinstitute.org>
2024-12-18 12:08:03 -0500

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,26 +5,26 @@ using Korg, Test, Logging, HDF5, ForwardDiff, FiniteDiff, Aqua
     # tools for testing: assert_allclose and assert_allclose_grid
     include("utilities.jl")
 
-    #include("wavelengths.jl")
-    #include("molecular_cross_sections.jl")
-    #include("cubic_splines.jl")
-    #include("transfer.jl")
-    #include("species.jl")
-    #include("interval.jl")
-    #include("continuum_absorption.jl") # test this after the "Interval" testset
-    #include("partition_funcs.jl")
-    #include("statmech.jl")
+    include("wavelengths.jl")
+    include("molecular_cross_sections.jl")
+    include("cubic_splines.jl")
+    include("transfer.jl")
+    include("species.jl")
+    include("interval.jl")
+    include("continuum_absorption.jl") # test this after the "Interval" testset
+    include("partition_funcs.jl")
+    include("statmech.jl")
     include("linelist.jl")
-    #include("fit.jl")                            # slow
-    #include("autodiff.jl")                       # slow
-    #include("autodiffable_conv.jl")
-    #include("atmosphere.jl")                     # slow
-    #include("abundances.jl")
-    #include("synthesize.jl")
-    #include("prune_linelist.jl")
-    #include("utils.jl")
-    #include("line_absorption.jl")
-    #include("qfactors.jl")
+    include("fit.jl")                            # slow
+    include("autodiff.jl")                       # slow
+    include("autodiffable_conv.jl")
+    include("atmosphere.jl")                     # slow
+    include("abundances.jl")
+    include("synthesize.jl")
+    include("prune_linelist.jl")
+    include("utils.jl")
+    include("line_absorption.jl")
+    include("qfactors.jl")
     @testset "Aqua automated checks" begin
         # see https://github.com/JuliaTesting/Aqua.jl/issues/77 for why I'm doing it this way,
         # (basically the default bahavior is different and this is what we want to avoid errors in
