24f6633f92454eb68a736f300e03fb4cef3e0196
Adam Wheeler <adamwhlr@gmail.com>
2025-06-03 13:17:14 -0400

    partially updated tutorial; export SynthesisResult

--- src/Korg.jl
+++ src/Korg.jl
@@ -29,7 +29,7 @@ include("qfactors.jl")                 # formalism to compute theoretical RV pre
 
 @compat public get_APOGEE_DR17_linelist, get_GALAH_DR3_linelist, get_GES_linelist, save_linelist,
                Fit, apply_LSF, compute_LSF_matrix, air_to_vacuum, vacuum_to_air, line_profile,
-               blackbody, prune_linelist, merge_close_lines, Line
+               blackbody, prune_linelist, merge_close_lines, Line, SynthesisResult
 export synthesize, synth, read_linelist, load_ExoMol_linelist, read_model_atmosphere,
        interpolate_marcs, format_A_X
 end # module
