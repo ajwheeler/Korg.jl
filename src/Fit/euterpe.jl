914056e1895db576a8bb7764c841ae741f0adce6
Adam Wheeler <adamwhlr@gmail.com>
2025-07-02 13:34:31 -0400

    very rough draft of multi-method line-by-line abundances

--- src/Fit/euterpe.jl
+++ src/Fit/euterpe.jl
@@ -5,7 +5,7 @@ TODO
 """
 module Euterpe
 using ..Korg
-using Statistics: std
+using Statistics: mean, std
 using Interpolations: linear_interpolation
 using Trapz: trapz
 
@@ -29,34 +29,59 @@ function abund(linelist, element, lines_to_fit, obs_flux, obs_wls, R,
                                                                           merged_windows,
                                                                           0)
     obs_flux = obs_flux[obs_wl_mask]
+    err = err[obs_wl_mask]
     obs_wls = obs_wls[obs_wl_mask]
+    line_indices = map(lines_to_fit) do λ
+        # TODO this is dumb
+        searchsortedfirst(obs_wls, λ - 0.2):searchsortedfirst(obs_wls, λ + 0.2)
+    end
 
     model_spectra = map(abundance_perturbations) do ΔA
         # TODO symbol/str nonsense
         element_specification = Dict(Symbol(element) => ΔA) # TODO solar is implicit here
         @time LSF * synth(; wavelengths=synthesis_wls, element_specification..., synth_kwargs...)[2]
     end
-    model_spectra = hcat(model_spectra...)
 
     # TODO better
-    pseudocontinuum_mask = (model_spectra[:, end] - model_spectra[:, 1]) .< 0.01 # TODO kwarg
+    pseudocontinuum_mask = abs.(model_spectra[end] - model_spectra[1]) .< 0.03 # TODO kwarg
+    model_pseudocontinua = calculate_pseudocontinuum.(Ref(obs_wls), model_spectra,
+                                                      Ref(pseudocontinuum_mask))
+    obs_pseudocontinuum = calculate_pseudocontinuum(obs_wls, obs_flux, pseudocontinuum_mask)
 
-    @time rough_EWs = map(eachcol(model_spectra)) do model_spectrum
-        calculate_rough_EWs(obs_wls, model_spectrum, pseudocontinuum_mask, lines_to_fit)
-    end
-    @time obs_rough_EWs = calculate_rough_EWs(obs_wls, obs_flux, pseudocontinuum_mask, lines_to_fit)
+    rough_EWs = calculate_rough_EWs.(Ref(obs_wls), model_spectra, model_pseudocontinua,
+                                     Ref(line_indices))
+    obs_rough_EW = calculate_rough_EWs(obs_wls, obs_flux, obs_pseudocontinuum, line_indices)
 
-    (; obs_wl_mask, model_spectra, pseudocontinuum_mask, rough_EWs, obs_rough_EWs)
+    chi2 = [sum(((obs_flux .- m) / err) .^ 2) for m in model_spectra]
+
+    model_depths = calculate_line_core_depths.(model_spectra, Ref(line_indices))
+    obs_depths = calculate_line_core_depths(obs_flux, line_indices)
+
+    (; obs_wl_mask, model_spectra, pseudocontinuum_mask, rough_EWs, obs_rough_EW, line_indices,
+     model_pseudocontinua, chi2, model_depths, obs_depths)
 end
 
-function calculate_rough_EWs(obs_wls, spectrum, pseudocontinuum_mask, lines_to_fit)
-    pseudocontinuum = linear_interpolation(obs_wls[pseudocontinuum_mask],
-                                           spectrum[pseudocontinuum_mask]; extrapolation_bc=1.0)(obs_wls)
+function calculate_pseudocontinuum(obs_wls, flux, pseudocontinuum_mask)
+    linear_interpolation(obs_wls[pseudocontinuum_mask], flux[pseudocontinuum_mask];
+                         extrapolation_bc=1.0)(obs_wls)
+end
+
+function calculate_rough_EWs(obs_wls, spectrum, pseudocontinuum, line_indices)
     absorption = pseudocontinuum - spectrum
-    map(lines_to_fit) do λ
-        # TODO THIS IS DUMB
-        r = searchsortedfirst(obs_wls, λ - 0.2):searchsortedfirst(obs_wls, λ + 0.2)
+    map(line_indices) do r
         trapz(obs_wls[r], absorption[r]) * 1e3 # convert to mÅ
+    end
+end
+
+function calculate_line_core_depths(spectrum, line_indices)
+    map(line_indices) do r
+        # get the middle 5 pixels in the range
+        # TODO everthing about this is terrible
+        firstind = r[1] + length(r) ÷ 2 - 2
+        lastind = firstind + 4
+        firstind = max(1, firstind)
+        lastind = min(length(spectrum), lastind)
+        mean(spectrum[firstind:lastind])
     end
 end
 
