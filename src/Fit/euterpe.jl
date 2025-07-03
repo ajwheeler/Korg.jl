f29e7f021b5b304d7bce3f97409783fd7a905e9e
Adam Wheeler <adamwhlr@gmail.com>
2025-07-03 13:54:04 -0400

    now with reporting of the best-fit abundance from each method

--- src/Fit/euterpe.jl
+++ src/Fit/euterpe.jl
@@ -41,24 +41,60 @@ function abund(linelist, element, lines_to_fit, obs_flux, obs_wls, R,
         element_specification = Dict(Symbol(element) => ΔA) # TODO solar is implicit here
         @time LSF * synth(; wavelengths=synthesis_wls, element_specification..., synth_kwargs...)[2]
     end
+    model_spectra = hcat(model_spectra...) # shape (n_wls, n_models)
 
     # TODO better
-    pseudocontinuum_mask = abs.(model_spectra[end] - model_spectra[1]) .< 0.03 # TODO kwarg
-    model_pseudocontinua = calculate_pseudocontinuum.(Ref(obs_wls), model_spectra,
-                                                      Ref(pseudocontinuum_mask))
+    pseudocontinuum_mask = abs.(model_spectra[:, end] - model_spectra[:, 1]) .< 0.03 # TODO kwarg
+    model_pseudocontinua = [calculate_pseudocontinuum(obs_wls, m, pseudocontinuum_mask)
+                            for m in eachcol(model_spectra)]
+    model_pseudocontinua = hcat(model_pseudocontinua...)
     obs_pseudocontinuum = calculate_pseudocontinuum(obs_wls, obs_flux, pseudocontinuum_mask)
 
-    rough_EWs = calculate_rough_EWs.(Ref(obs_wls), model_spectra, model_pseudocontinua,
-                                     Ref(line_indices))
-    obs_rough_EW = calculate_rough_EWs(obs_wls, obs_flux, obs_pseudocontinuum, line_indices)
+    EWs = map(eachcol(model_spectra), eachcol(model_pseudocontinua)) do m, p
+        calculate_rough_EWs(obs_wls, m, p, line_indices)
+    end
+    EWs = hcat(EWs...)
+    obs_EWs = calculate_rough_EWs(obs_wls, obs_flux, obs_pseudocontinuum, line_indices)
+    EW_itps = interpolate_and_predict(abundance_perturbations, obs_EWs, EWs)
+    EW_As = [itp(EW) for (itp, EW) in zip(EW_itps, obs_EWs)]
 
-    chi2 = [sum(((obs_flux .- m) / err) .^ 2) for m in model_spectra]
+    zscores = ((obs_flux .- model_spectra) ./ err) .^ 2
+    chi2 = Matrix{Float64}(undef, length(line_indices), length(abundance_perturbations))
+    for (i, r) in enumerate(line_indices)
+        chi2[i, :] .= sum(zscores[r, :]; dims=1)[:]
+    end
+    chi2_itps, chi2_coeffs = quadratic_minimizers(abundance_perturbations, chi2)
+    chi2_As = -chi2_coeffs[2, :] ./ 2chi2_coeffs[3, :]
 
-    model_depths = calculate_line_core_depths.(model_spectra, Ref(line_indices))
+    depths = [calculate_line_core_depths(m, line_indices) for m in eachcol(model_spectra)]
+    depths = hcat(depths...)
     obs_depths = calculate_line_core_depths(obs_flux, line_indices)
+    depth_itps = interpolate_and_predict(abundance_perturbations, obs_depths, depths)
+    depth_As = [itp.(d) for (itp, d) in zip(depth_itps, obs_depths)]
 
-    (; obs_wl_mask, model_spectra, pseudocontinuum_mask, rough_EWs, obs_rough_EW, line_indices,
-     model_pseudocontinua, chi2, model_depths, obs_depths)
+    (; EW_As, depth_As, chi2_As, obs_wl_mask, model_spectra, pseudocontinuum_mask, EWs,
+     obs_EWs, EW_itps, line_indices, model_pseudocontinua, chi2, chi2_coeffs, chi2_itps,
+     depths, obs_depths, depth_itps)
+end
+
+"""
+Fit a quadratic model to chi2(abundance_perturbation), and report the minimizer
+"""
+function quadratic_minimizers(abundance_perturbations, chi2)
+    # fit a quadratic model
+    A = hcat((abundance_perturbations .^ i for i in 0:2)...)
+    coeffs = A \ chi2'
+    map(eachcol(coeffs)) do c
+        x -> sum(c .* x .^ (0:2))
+    end, coeffs
+end
+
+function interpolate_and_predict(abundance_perturbations, observed_quantities, model_quantities)
+    map(eachrow(model_quantities)) do quantities
+        A = hcat((quantities .^ i for i in 0:3)...)
+        coeffs = A \ abundance_perturbations
+        x -> sum(coeffs .* x .^ (0:3))
+    end
 end
 
 function calculate_pseudocontinuum(obs_wls, flux, pseudocontinuum_mask)
