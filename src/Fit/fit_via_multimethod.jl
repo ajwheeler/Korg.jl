fc3638f98f0222621460e403a94be17e6b7a2a77
Adam Wheeler <adamwhlr@gmail.com>
2025-07-25 15:25:20 -0400

    get it in better shape

--- src/Fit/fit_via_multimethod.jl
+++ src/Fit/fit_via_multimethod.jl
@@ -1,9 +1,3 @@
-"""
-Kinda like Bacchus.
-
-TODO
-"""
-module Euterpe
 using ..Korg
 using Statistics: mean, std
 using Interpolations: linear_interpolation
@@ -18,39 +12,52 @@ TODO make sure solar abundances are flexible. I think they currently are.
 # Keyword arguments:
 
   - `verbose` (default: `true`: whether to print progress information
+
+# TODO kwarg everything?
 """
-function multi_method_abundances(linelist, element, lines_to_fit, obs_flux, obs_wls, R,
-                                 err=ones(length(obs_flux));
-                                 verbose=true,
-                                 abundance_perturbations=-0.6:0.3:0.6, synth_kwargs...)
+function multimethod_abundances(linelist, element, guess, lines_to_fit, obs_flux, obs_wls, R,
+                                err=ones(length(obs_flux)); verbose=true,
+                                abundance_perturbations=-0.6:0.3:0.6, synth_kwargs...)
     # TODO check no conflicting R specification
+    # check that lines_to_fit are within the range of obs_wls
 
     # wavelengths, windows and LSF, TODO refactor
     windows = [(l - 1, l + 1) for l in lines_to_fit] # TODO kwarg
-    merged_windows, lines_per_window = Korg.merge_bounds(windows, 0.0)
+    merged_windows, _ = Korg.merge_bounds(windows, 0.0)
     synthesis_wls, obs_wl_mask, LSF = Korg.Fit._setup_wavelengths_and_LSF(obs_wls,
                                                                           nothing,
                                                                           nothing, R,
                                                                           merged_windows,
                                                                           0)
+
+    if !issorted(lines_to_fit)
+        lines_to_fit = sort(lines_to_fit)
+    end
+    if lines_to_fit[1] < obs_wls[1] || lines_to_fit[end] > obs_wls[end]
+        throw(ArgumentError("lines_to_fit includes lines outside of obs_wls"))
+    end
+
     obs_flux = obs_flux[obs_wl_mask]
     err = err[obs_wl_mask]
     obs_wls = obs_wls[obs_wl_mask]
     line_indices = map(lines_to_fit) do λ
-        # TODO this is dumb
-        searchsortedfirst(obs_wls, λ - 0.2):searchsortedfirst(obs_wls, λ + 0.2)
+        # this is dumb
+        searchsortedfirst(obs_wls, λ - 0.2):searchsortedlast(obs_wls, λ + 0.2)
     end
 
     linelist = Korg.filter_linelist(linelist, synthesis_wls, 10.0; warn_about_time=false)
+    abundances = abundance_perturbations .+ guess
 
-    #@time model_spectra = map(A_Xs, atms) do A_X, atm
-    model_spectra = @showprogress "synthesizing" map(abundance_perturbations) do ΔA
-
-        # TODO symbol/str nonsense, how to do this for any element?
-        #element_specification = Dict(Symbol(element) => ΔA) # TODO solar is implicit here
-        #@time LSF * synth(; wavelengths=synthesis_wls, element_specification..., synth_kwargs...)[2]
-
-        LSF * synth(; linelist=linelist, wavelengths=synthesis_wls, m_H=ΔA, synth_kwargs...)[2]
+    # would be cool to multithread
+    model_spectra = @showprogress "synthesizing" ennabled=verbose map(abundances) do X_H
+        params = Dict([
+                          :wavelengths => synthesis_wls,
+                          :linelist => linelist,
+                          Symbol(element) => X_H,
+                          synth_kwargs...
+                      ])
+        flux = synth(; params...)[2]
+        LSF * flux
     end
     model_spectra = hcat(model_spectra...) # shape (n_wls, n_models)
 
@@ -66,22 +73,22 @@ function multi_method_abundances(linelist, element, lines_to_fit, obs_flux, obs_
     end
     EWs = hcat(EWs...)
     obs_EWs = calculate_rough_EWs(obs_wls, obs_flux, obs_pseudocontinuum, line_indices)
-    EW_itps = interpolate_and_predict(abundance_perturbations, obs_EWs, EWs)
+    EW_itps = interpolate_and_predict(abundances, obs_EWs, EWs)
     EW_As = [itp(EW) for (itp, EW) in zip(EW_itps, obs_EWs)]
 
     zscores = ((obs_flux .- model_spectra) ./ err) .^ 2
-    chi2 = Matrix{Float64}(undef, length(line_indices), length(abundance_perturbations))
+    chi2 = Matrix{Float64}(undef, length(line_indices), length(abundances))
     for (i, r) in enumerate(line_indices)
         chi2[i, :] .= sum(zscores[r, :]; dims=1)[:]
     end
-    chi2_itps, chi2_coeffs = quadratic_minimizers(abundance_perturbations, chi2)
+    chi2_itps, chi2_coeffs = quadratic_minimizers(abundances, chi2)
     chi2_As = -chi2_coeffs[2, :] ./ 2chi2_coeffs[3, :]
 
     depths = [calculate_line_core_depths(m, pc, line_indices)
               for (m, pc) in zip(eachcol(model_spectra), eachcol(model_pseudocontinua))]
     depths = hcat(depths...)
     obs_depths = calculate_line_core_depths(obs_flux, obs_pseudocontinuum, line_indices)
-    depth_itps = interpolate_and_predict(abundance_perturbations, obs_depths, depths)
+    depth_itps = interpolate_and_predict(abundances, obs_depths, depths)
     depth_As = [itp.(d) for (itp, d) in zip(depth_itps, obs_depths)]
 
     (; EW_As, depth_As, chi2_As, obs_wl_mask, model_spectra, pseudocontinuum_mask,
@@ -131,6 +138,4 @@ function calculate_line_core_depths(spectrum, pseudocontinuum, line_indices)
         lastind = min(length(spectrum), lastind)
         mean(spectrum[firstind:lastind] - pseudocontinuum[firstind:lastind])
     end
-end
-
 end
