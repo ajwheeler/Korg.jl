5ca7b6ae06658162af30c0ebc22f1633a6b927d5
Adam Wheeler <adamwhlr@gmail.com>
2026-07-11 12:58:55 -0400

    uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,30 +11,30 @@
 include("imports.jl")
 
 @testset "Korg tests" verbose=true showtiming=true begin
-    #include("wavelengths.jl")
-    #include("molecular_cross_sections.jl")
-    #include("cubic_splines.jl")
-    #include("transfer.jl")
-    #include("species.jl")
-    #include("interval.jl")
-    #include("continuum_absorption.jl") # test this after the "Interval" testset
-    #include("statmech.jl")
-    #include("linelist.jl")
+    include("wavelengths.jl")
+    include("molecular_cross_sections.jl")
+    include("cubic_splines.jl")
+    include("transfer.jl")
+    include("species.jl")
+    include("interval.jl")
+    include("continuum_absorption.jl") # test this after the "Interval" testset
+    include("statmech.jl")
+    include("linelist.jl")
     include("fit.jl")                            # slow
-    #include("autodiff.jl")                       # slow
-    #include("autodiffable_conv.jl")
-    #include("newton.jl")
-    #include("atmosphere.jl")                     # slow
-    #include("abundances.jl")
-    #include("synthesize.jl")
-    #include("synth.jl")
-    #include("prune_linelist.jl")
-    #include("utils.jl")
-    #include("line_absorption.jl")
-    #include("qfactors.jl")
-    #@testset "Aqua automated checks" begin
-    #    # unbound args has a false positive for Korg.Line because it's using a heuristic.  That
-    #    # constructor should probably be less convoluted anyway, but for now skip those checks.
-    #    Aqua.test_all(Korg; unbound_args=false)
-    #end
+    include("autodiff.jl")                       # slow
+    include("autodiffable_conv.jl")
+    include("newton.jl")
+    include("atmosphere.jl")                     # slow
+    include("abundances.jl")
+    include("synthesize.jl")
+    include("synth.jl")
+    include("prune_linelist.jl")
+    include("utils.jl")
+    include("line_absorption.jl")
+    include("qfactors.jl")
+    @testset "Aqua automated checks" begin
+        # unbound args has a false positive for Korg.Line because it's using a heuristic.  That
+        # constructor should probably be less convoluted anyway, but for now skip those checks.
+        Aqua.test_all(Korg; unbound_args=false)
+    end
 end #top-level testset
